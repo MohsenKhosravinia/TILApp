18cd6254a12326845d7c125d63396d0585f09e96
Mohsen Khosravinia <khosravinia.mohsen@gmail.com>
2024-01-06 20:50:04 +0330

    Add index and acronym leaves

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -3,14 +3,36 @@ import Vapor
 struct WebsiteController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.get(use: indexHandler)
+        routes.get("acronyms", ":acronymID", use: acronymHandler)
     }
     
     private func indexHandler(_ req: Request) throws -> EventLoopFuture<View> {
-        let context = IndexContext(title: "Homepage")
-        return req.view.render("index", context)
+        Acronym.query(on: req.db).all().flatMap { acronyms in
+            let context = IndexContext(title: "Homepage", acronyms: acronyms.isEmpty ? nil : acronyms)
+            return req.view.render("index", context)
+        }
+    }
+    
+    private func acronymHandler(_ req: Request) throws -> EventLoopFuture<View> {
+        Acronym
+            .find(req.parameters.get("acronymID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { acronym in
+                acronym.$user.get(on: req.db).flatMap { user in
+                    let context = AcronymContext(title: acronym.long, acronym: acronym, user: user)
+                    return req.view.render("acronym", context)
+                }
+            }
     }
 }
 
 struct IndexContext: Encodable {
     let title: String
+    let acronyms: [Acronym]?
+}
+
+struct AcronymContext: Encodable {
+    let title: String
+    let acronym: Acronym
+    let user: User
 }
